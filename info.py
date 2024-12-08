b4daf9e7e5ea031699bc481dade33b1c239b0eb5
saikatsingh <mailmetosaikat676@gmail.com>
2024-12-08 17:17:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -111,7 +111,7 @@ SHORTLINK_API = environ.get('SHORTLINK_API', '')
 
 # Others
 MAX_B_TN = environ.get("MAX_B_TN", "5")
-PORT = environ.get("PORT", "9019") # If U Use HostingUp VPS Server Then U Recived In Mail Hosting Port (Only For Stream Feture) 
+PORT = environ.get("PORT", "8080") # If U Use HostingUp VPS Server Then U Recived In Mail Hosting Port (Only For Stream Feture) 
 MSG_ALRT = environ.get('MSG_ALRT', 'Hello My Dear Friends ❤️')
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
@@ -132,7 +132,7 @@ YEARS = ["1900", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998",
 
 
 # Online Stream and Download
-STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or False
+STREAM_MODE = bool(environ.get('STREAM_MODE', False)) # Set True or False
 
 # If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 MULTI_CLIENT = False
